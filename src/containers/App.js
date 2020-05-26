3a629199ec96016f78eb7441f6c9caeb04a22cef
yusufcodes <yusufcodes@outlook.com>
2020-05-26 12:22:07 +0100

    Redone the folder structure

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import classes from './App.css';
-import Person from './Person/Person';
+import Person from '../components/Persons/Person/Person';
 
 class App extends Component {
   state = {
